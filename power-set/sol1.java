bf4be08956b2dd43753cd3b66089c670389be3c0
Divyansh Sharma <divyanshsharmakotarajasthan@gmail.com>
2026-08-09 23:33:49 +0530

    power set

---
+++ power-set/sol1.java
@@ -0,0 +1,50 @@
+// Using Bitmasking 
+// Take every possible subset upto 2^n and compare its digits 
+// if it is 1 then add that char in ans , else not 
+
+import java.util.ArrayList;
+
+class Solution{
+    private String s;
+    ArrayList<String>subsequences ;
+
+    Solution(String s){
+        this.s = s;
+        this.subsequences = new ArrayList<>();
+
+    }
+
+    public ArrayList<String> main(){
+        int n = s.length();
+        int total = 1 << n;
+      
+        for( int mask = 0 ; mask < total ; mask++){
+              StringBuilder res = new StringBuilder();
+            for (int i = 0 ; i < n ; i++){
+                if (((mask) &  (1<<i)) != 0){
+                    res.append(s.charAt(i));
+                }
+            }
+            subsequences.add(res.toString());
+
+        }
+        return subsequences;
+
+
+    }
+
+
+    
+ }
+
+
+public class sol1 {
+    static void main(){
+        String s = "abcd";
+        new ArrayList<>();
+        Solution solution = new Solution(s);
+        ArrayList<String> ans = solution.main();
+        System.out.println(ans) ;
+
+    }
+}
