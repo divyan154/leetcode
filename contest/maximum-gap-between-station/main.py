322668ea2fa435a48d1a0b3679359854cdc39038
Divyansh Sharma <divyanshsharma@Divyanshs-MacBook-Air.local>
2026-08-17 13:58:15 +0530

    Problem solution

---
+++ contest/maximum-gap-between-station/main.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maximumGap(self, skill: str, station: str) -> int:
+        n = len(skill)
+        m = len(station)
+        leftMostPosition = [0] * n
+        rightMostPostion = [0] * n
+        
+        j = 0
+        for i in range(n):
+            print(i)
+            while skill[i] != station[j] :
+                j += 1
+            leftMostPosition[i] = j
+            j += 1   
+
+        #  Right Most Position
+        j = m - 1
+        for i in range(n - 1, -1, -1):
+            while skill[i] != station[j]:
+                j -= 1
+            rightMostPostion[i] = j
+            j -= 1
+        
+        ans = 0
+        for i in range(n - 1): 
+            ans = max(ans, rightMostPostion[i + 1] - leftMostPosition[i])
+        return ans    
+
+
+            
+
