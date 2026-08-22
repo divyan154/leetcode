0d9974ac4670f8af372bfe3157e96689ea13a9d0
Divyansh Sharma <divyanshsharmakotarajasthan@gmail.com>
2026-08-22 22:49:33 +0530

    number divisible solution

---
+++ potd/divisible-by-sum-and-product/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        prod , tot , num = 1, 0, n
+        
+        while n != 0:
+            rem = n % 10
+            prod *= rem
+            tot += rem
+            n = n // 10
+        print(tot)
+        print(prod)
+        return True if num % (prod + tot) == 0 else False 
+        
