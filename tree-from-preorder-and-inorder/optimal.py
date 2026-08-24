8f6d53cdbf564bc1718b06d1f754e84693cb8812
Divyansh Sharma <divyanshsharmakotarajasthan@gmail.com>
2026-08-25 02:39:53 +0530

    Solution approach 2

---
+++ tree-from-preorder-and-inorder/optimal.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def build(self, preorder : deque, mapping, start, end):
+
+        if start <= end:
+            root_val = preorder.popleft()
+            node = TreeNode(root_val)
+            idx = mapping[root_val]
+
+            node.left = self.build(preorder, mapping, start, idx - 1)
+            node.right = self.build(preorder, mapping, idx + 1, end)
+            return node
+        return None    
+
+
+
+
+
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        n = len(inorder)
+        preorder = deque(preorder)
+        
+        mapping = {}
+        for i in range(n):
+            mapping[inorder[i]] = i
+
+        return self.build(preorder, mapping, 0, n - 1)
+
+
+        
