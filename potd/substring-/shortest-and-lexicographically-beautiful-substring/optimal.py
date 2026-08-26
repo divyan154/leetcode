1b5a8d8455810962fe1e66136ae6ce11e1edc1f5
Divyansh Sharma <divyanshsharmakotarajasthan@gmail.com>
2026-08-26 15:08:49 +0530

    substrin brute

---
+++ potd/substring-/shortest-and-lexicographically-beautiful-substring/optimal.py
@@ -0,0 +1,31 @@
+class Solution:
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        n = len(s)
+        mini = float('inf')
+        for i in range(n):
+            temp = ""
+            oneCount = 0
+            for j in range(i , n):
+                temp += s[j]
+                if s[j] == '1':
+                    oneCount += 1
+                if oneCount == k :
+                    mini = min(mini,len(temp)) 
+                elif oneCount > k:
+                    break       
+        if mini == float('inf'):
+            return ""         
+        res = []
+        for i in range(n):
+            temp = ""
+            oneCount = 0
+            for j in range(i, n):
+                temp += s[j]
+                if s[j] == '1':
+                    oneCount += 1
+                if oneCount == k and len(temp) == mini:
+                    res.append(temp)  
+                elif oneCount > k:
+                    break      
+
+        return min(res)
