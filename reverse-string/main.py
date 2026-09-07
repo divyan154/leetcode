b8ef56b7db2f7b5baa117d31fc2075b76e5d903b
divyan154 <divyanshsharmakotarajasthan@gmail.com>
2026-09-07 15:42:15 +0530

    reverse a string soln

---
+++ reverse-string/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseString(self, s: str) -> str:
+        n = len(s)
+        s = list(s)
+        i , j = 0 , n - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        return "".join(s)    
+        
